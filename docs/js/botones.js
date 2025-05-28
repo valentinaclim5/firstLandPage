9ecab6879a8072d17133c58e9841ad7b814deff7
valentinaclim5 <noahvalentina.aguirre.l@gmail.com>
2025-05-28 12:58:50 +0200

    modificación de Practicas.html, index.html y eliminación de js/botones.js

--- docs/js/botones.js
+++
@@ -1,8 +0,0 @@
-const scrollToTopButton = document.getElementById('scroll-to-top');
-
-scrollToTopButton.addEventListener('click', () => {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth' // Para una animación suave
-  });
-});
